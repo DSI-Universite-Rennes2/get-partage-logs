1401737d787a7b1de086ee3226917c7e6da660b2
Yann 'Ze' Richard <yann.richard@univ-rennes2.fr>
2022-12-08 12:11:31 +0100

    First commit
    Signed-off-by: Yann 'Ze' Richard <yann.richard@univ-rennes2.fr>

---
+++ README.md
@@ -0,0 +1,39 @@
+# Logs de PARTAGE
+
+[![reuse compliant](https://reuse.software/badge/reuse-compliant.svg)](https://reuse.software/) 
+[![Trigger: Shell Check](https://github.com/DSI-Universite-Rennes2/get-partage-logs/actions/workflows/main.yml/badge.svg?event=push)](https://github.com/DSI-Universite-Rennes2/get-partage-logs/actions/workflows/main.yml)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+Ce script permet de rapatrier les logs de PARTAGE :
+
+* télécharge les logs compressés non existants en local
+* télécharge les logs du jour en mode incrémentiel
+* Efface les logs de plus de $MAX_LOG_DAYS jours
+
+Vous pouvez ainsi lancer ce script autant de fois qu'il vous plaira et à la fréquence de votre choix sans risques d'avoir des centaines de gigaoctets transférés. Seul ce qui manque est transféré.
+
+## Utilisation
+
+### Dépendance
+
+Vous devez avoir `lftp` installé sur votre système
+
+### Configuration
+
+Copiez le fichier `config-partage-log-dist` en `config-partage-log` et adaptez les variables à votre environnement.
+
+## Contribute
+
+See [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the [GNU General Public License v3.0 or later](LICENSE)
+as published by the Free Software Foundation.
+
+The program in this repository meet the requirements to be REUSE compliant,
+meaning its license and copyright is expressed in such as way so that it
+can be read by both humans and computers alike.
+
+For more information, see https://reuse.software/
---
+++ get-partage-logs.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# Rapatriement des logs de PARTAGE
+#
+# -----------------------------------------------------------------------------
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE 
+#
+# Authors :
+#
+#   - Yann 'Ze' Richard <yann.richard à univ-rennes2.fr> - DSI Université Rennes 2
+#
+# -----------------------------------------------------------------------------
+PATH="/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin"
+LDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echoerr() { echo "$@" 1>&2; }
+function command_exists () { 
+    command -v "$1" >/dev/null 2>&1; 
+}
+
+TMPDIR=$(mktemp -d -t 'get-partage-logs.XXXXXX')
+if [[ ! "$TMPDIR" || ! -d "$TMPDIR" ]]
+then
+    echoerr "Could not create temp dir"
+    exit 1
+fi
+trap 'rm -rf "$TMPDIR"' EXIT
+
+# -----------------------------------------------------------------------------
+# Chargement de la configuration + verifications
+#
+# Doit définir : 
+#   LOGUSER="leuser"
+#   LOGHOST="leserveurdelogdepartage.renater.fr"
+#   DESTDIR_LOG_NG=/la/ou/tu/veux/tes/logs/partage
+#   SSH_KEY=/la/ou/est/la/ssh-key-partage
+#   MAX_LOG_DAYS=365   
+# shellcheck disable=SC1090
+. "$LDIR/config-get-partage-log"
+if [ -z "$LOGUSER" ]
+then
+    echoerr "LOGUSER is not defined..."
+    exit 1
+fi
+if [ -z "$DESTDIR_LOG_NG" ] 
+then
+    echoerr "DESTDIR_LOG_NG is not defined or empty..."
+    exit 1
+fi
+if [ "$DESTDIR_LOG_NG" == "/" ]
+then
+    echoerr "DESTDIR_LOG_NG is equal to '/' ?!"
+    exit 1
+fi
+if ! command_exists "lftp"
+then
+    echoerr "lftp is not installed"
+    exit 1
+fi
+
+# -----------------------------------------------------------------------------
+TODAY=$(date +%Y-%m-%d)
+cat <<EOF >> "$TMPDIR/lftp-script"
+    set sftp:auto-confirm yes
+    set sftp:connect-program "ssh -a -x -i $SSH_KEY"
+    set mirror:parallel-directories true
+    #set xfer:log true
+    #set xfer:log-file "/tmp/renater-partage-ng.log"
+    open sftp://${LOGUSER}:FAKEPWD@${LOGHOST}:10022 
+    # logs compressées
+    mirror --use-cache --continue --parallel=10 --scan-all-first --include=".*.log.gz" /logs/ ${DESTDIR_LOG_NG}/
+    # logs du jour
+    lcd "${DESTDIR_LOG_NG}/${TODAY}"
+    mget -c --parallel=10 /logs/${TODAY}/*.log
+    bye
+EOF
+lftp -f "$TMPDIR/lftp-script" > /dev/null
+
+# Remove des .log plus vieux de 1 jours (Il y aura la version compressée)
+YESTERDAY=$(date -d "yesterday" '+%Y%m%d2359.59')
+touch -t "$YESTERDAY" "$TMPDIR/yesterday"
+find "${DESTDIR_LOG_NG}" -type f -name "*.log" -not -newer "$TMPDIR/yesterday" -delete
+
+# Remove des logs plus vieux que MAX_LOG_DAYS
+RE_INT='^[0-9]+$'
+if [[ $MAX_LOG_DAYS =~ $RE_INT ]]
+then
+    find "${DESTDIR_LOG_NG}" -type f -name "*.log" -mtime +"${MAX_LOG_DAYS}"
+else
+    echoerr "MAX_LOG_DAYS not defined or not a digit (${MAX_LOG_DAYS})"
+fi
